87cb38de80c05b84f3ffa362870c43b4d9fd2aed
Matteo Spada <matteo.spada@ymail.com>
2022-08-13 11:39:44 +0200

    Initial Commit

---
+++ Sparkle-test/Sparkle_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sparkle_testApp.swift
+//  Sparkle-test
+//
+//  Created by Matteo Spada on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Sparkle_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
